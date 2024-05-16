da84d96ef2f782e4010cf19bf6a3b476d5864119
Mukesh <148268845+dry5kulll@users.noreply.github.com>
2024-05-16 20:06:22 +0530

    Update and rename Variables.sh to Basic Operation.sh

---
+++ 1. Variables/Basic Operation.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Declare variables and assign values
+num1=10
+num2=2
+
+
+# Perform arithmetic operations
+addition=$((num1+num2))
+subtraction=$((num1-num2))
+product=$((num1*num2))
+division=$((num1/num2))
+modulus=$((num1%num2))
+
+
+# Display results
+echo Addition: $addition
+echo Subtraction: $subtraction
+echo Product: $product
+echo Division: $division
+echo Modulus: $modulus
