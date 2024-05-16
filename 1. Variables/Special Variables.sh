8cd6bd10f73d335ba0d878bc3fda51cb4cd534c7
Mukesh <148268845+dry5kulll@users.noreply.github.com>
2024-05-17 01:11:05 +0530

    Create Special Variables.sh

---
+++ 1. Variables/Special Variables.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Name of the Script
+echo "Script Name: $0"
+
+# Print process ID of current shell
+echo "Process ID: $$"
+
+# Exit Status of last command
+echo "Exit Status: $?"
+
+# Total Number of arguments
+echo "Total Number of Arguments: $#"
