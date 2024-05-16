12725e879cb78b019020c61bf93216346901efc7
Mukesh <148268845+dry5kulll@users.noreply.github.com>
2024-05-16 20:14:25 +0530

    Update Simple.sh

--- String Manipulation/Simple.sh
+++ String Manipulation/Simple.sh
@@ -1,1 +1,16 @@
+#!/bin/bash
 
+# Declare and initialize string variable
+name="Adam Warlock"
+
+# Length of string
+length=${#name}
+echo Name Length: $length
+
+# Convert string to uppercase
+uppercase=$(echo $name | tr '[:lower:]' '[:upper:]')
+echo Uppercase Name: $uppercase
+
+# Extract Substring
+substring=${name:1:4}
+echo Substring: $substring
