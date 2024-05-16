13f82e35d0243f38d17b231834291568c5971d47
Mukesh <148268845+dry5kulll@users.noreply.github.com>
2024-05-17 00:43:52 +0530

    Update Even Odd.sh

--- Conditional Statement/Even Odd.sh
+++ Conditional Statement/Even Odd.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-# # Prompt user to enter an integer
+# Prompt user to enter an integer
 read -p "Enter an Integer: " num
 
-# # Check if number is even or odd
+# Check if number is even or odd
 if (( num % 2 == 0 )); then
     echo $num is even.
 else
