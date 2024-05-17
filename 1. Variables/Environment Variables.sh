42d56ef5258cbd4b9771832285504ef2ba425cbb
Mukesh <148268845+dry5kulll@users.noreply.github.com>
2024-05-17 14:29:03 +0530

    Update Environment Variables.sh

--- 1. Variables/Environment Variables.sh
+++ 1. Variables/Environment Variables.sh
@@ -1,1 +1,18 @@
+#!/bin/bash
+
+echo -e "\nUsername: $USERNAME"
+echo -e "\nHostname: $HOSTNAME"
+echo -e "\nPATH: $PATH"
+echo -e "\nHome Directory: $HOME"
+echo -e "\nSHELL: $SHELL"
+echo -e "\nPWD: $PWD"
+echo -e "\nOLDPWD: $OLDPWD"
+echo -e "\nTERM: $TERM"
+echo -e "\nBASH: $BASH"
+echo -e "\nOS: $OS"
+
+
+echo -e "\n\nThe number of seconds since the script was started: $SECONDS"
+echo -e "\nReturns a different random number each time is it referred to: $RANDOM"
+echo -e "\nReturns the current line number in the Bash script: $LINENO"
 
