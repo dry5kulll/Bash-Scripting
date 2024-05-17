c157ad42c62aa16443d5a0db160859efe7b5b2e7
Mukesh <148268845+dry5kulll@users.noreply.github.com>
2024-05-18 03:54:17 +0530

    Delete Datatypes.sh

--- Datatypes.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-
-# Associative arrays
-# -A : It is used to declare an associative array.
-declare -A colors
-colors=([red]="#FF000" [green]="#00FF00" [blue]="#0000FF")
-
-echo -e "\nAssociative Array Values : ${colors[@]}."
