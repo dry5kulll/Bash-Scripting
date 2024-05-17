306357dd7bed0b64fd9379a79d61f5f32ff626b9
Mukesh <148268845+dry5kulll@users.noreply.github.com>
2024-05-17 14:51:09 +0530

    Update Basic Operation.sh

--- 1. Variables/Basic Operation.sh
+++ 1. Variables/Basic Operation.sh
@@ -1,6 +1,31 @@
 #!/bin/bash
 
-# Declare variables and assign values
+# Declaring Variables (Method 1)
+declare name="Adam Warlock"
+echo "My name is $name"
+printf "Hi, I am %s, my home directory is %s & my default shell is %s.\n" "$USERNAME" "$HOME" "$SHELL"
+
+
+# Declaring Variables (Method 2)
+firstName="Adam"
+lastName="Warlock"
+age=35
+echo -e "\nName 1: $firstName $lastName"
+
+
+# Unsetting Variables
+unset firstName
+echo -e "\nAfter Unsetting First Name variable: "
+echo "Name 2: $firstName $lastName"
+echo "Age: $age"
+
+
+# Readonly Variable
+readonly PI=3.14
+echo -e "\nThe value of PI is $PI"
+
+
+# Basic Arithmentic Operation
 num1=10
 num2=2
 
